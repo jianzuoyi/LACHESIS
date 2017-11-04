07b2b6f399eeaa2500cb57e183995dcd0dba14f6
Zuoyi Jian <jianzuoyi@qq.com>
2017-11-04 23:29:21 +0800

    Initial commit

---
+++ env.sh
@@ -0,0 +1,4 @@
+export LACHESIS_BOOST_DIR=/opt/bio/boost_1_64_0
+#export LACHESIS_BOOST_DIR=/its1/GB_BT2/jianzuoyi/biosoft/boost_1_65_1
+export LACHESIS_SAMTOOLS_DIR=/its1/GB_BT2/jianzuoyi/pipeline/biosoft/01.Assembly/LACHESIS/src/samtools-0.1.19
+export SAMTOOLS_HOME=/its1/GB_BT2/jianzuoyi/pipeline/biosoft/01.Assembly/LACHESIS/src/samtools-0.1.19
